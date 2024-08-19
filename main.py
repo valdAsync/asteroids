1c174e80bda8afbef9bb14e5a356d444b4a00e58
valdAsync <valdasync@proton.me>
2024-08-19 22:34:25 +0300

    update

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -13,6 +16,8 @@ def main():
 
         pygame.Surface.fill(screen, color="black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
