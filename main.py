aa9de13bd5f0a4958bde0e5b8af12a3492952922
valdAsync <valdasync@proton.me>
2024-08-19 22:25:50 +0300

    update

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
