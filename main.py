78101985b77ffaaa3ba9cf08f39a7bfb9a40ae67
valdAsync <valdasync@proton.me>
2024-08-25 15:41:13 +0300

    destruction

--- main.py
+++ main.py
@@ -42,6 +42,11 @@ def main():
                 print("Game Over")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    shot.kill()
+                    asteroid.kill()
+
         screen.fill("black")
 
         for obj in drawable:
